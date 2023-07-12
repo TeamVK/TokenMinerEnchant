02280ffdb11d2b8570493281a6a0f1107be9b378
vk2gpz <vk2gpz@gmail.com>
2023-07-12 13:29:07 +1000

    version 3.0.0

--- src/main/java/my/domain/tokenenchant/enchants/TokenMiner.java
+++ src/main/java/my/domain/tokenenchant/enchants/TokenMiner.java
@@ -71,7 +71,7 @@ public class TokenMiner extends EnchantHandler {
 	
 	@Override
 	public String getVersion() {
-		return "1.0.0";
+		return "3.0.0";
 	}
 	
 	@EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
